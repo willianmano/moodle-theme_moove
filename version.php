cbcdeda95ff4e8bf20e6155b006e96bd38b97c85
Willian Mano <willianmanoaraujo@gmail.com>
2017-04-11 15:06:11 -0300

    Fix version file

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2017040600;
 $plugin->requires  = 2016112900;
 $plugin->component = 'theme_moove';
+$plugin->release   = '1.0.0';
+$plugin->maturity  = MATURITY_STABLE;
