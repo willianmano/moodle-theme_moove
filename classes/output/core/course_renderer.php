2d8c93aa401e4dce912780b90e4b978144c7b7e8
Willian Mano <willianmanoaraujo@gmail.com>
2017-07-14 14:40:55 -0300

    Remove issue of translation

--- classes/output/core/course_renderer.php
+++ classes/output/core/course_renderer.php
@@ -240,7 +240,7 @@ class course_renderer extends \core_course_renderer {
 
         $content .= html_writer::start_tag('div', array('class' => 'pull-right'));
         $content .= html_writer::link(new moodle_url('/course/view.php', array('id' => $course->id)),
-                            'ACESSAR', array('class' => 'card-link btn btn-default'));
+                            get_string('access', 'theme_moove'), array('class' => 'card-link btn btn-default'));
         $content .= html_writer::end_tag('div'); // End pull-right.
 
         $content .= html_writer::end_tag('div'); // End card-block.
