84558ca2d6a1056209b6f16407133625cd183e74
Willian Mano <willianmanoaraujo@gmail.com>
2017-08-02 14:31:26 -0300

    Readd the breadcrumbs to incourse pages

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017071400;
+$plugin->version   = 2017080100;
 $plugin->requires  = 2016112900;
 $plugin->component = 'theme_moove';
-$plugin->release   = '1.3.3';
+$plugin->release   = '1.3.4';
 $plugin->maturity  = MATURITY_STABLE;
