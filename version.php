af22d41a65213e127749f124a452839d2fc4ca2c
Willian Mano <willianmanoaraujo@gmail.com>
2017-08-03 17:46:26 -0300

    Resolves bug of version 331

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2017080100;
+$plugin->version = 2017080300;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2016112900;
@@ -36,7 +36,7 @@ $plugin->requires = 2016112900;
 $plugin->component = 'theme_moove';
 
 // This is the named version.
-$plugin->release = '1.3.4';
+$plugin->release = '1.3.5';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
