1eca9e441e85909a6a282e1725c578207cb7098b
Willian Mano <willianmanoaraujo@gmail.com>
2017-10-03 20:20:29 -0300

    Increases version number

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2017093000;
+$plugin->version = 2017100100;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2016112900;
@@ -36,7 +36,7 @@ $plugin->requires = 2016112900;
 $plugin->component = 'theme_moove';
 
 // This is the named version.
-$plugin->release = '1.4.3';
+$plugin->release = '1.4.4';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
