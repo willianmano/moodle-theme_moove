986de53ba41b6cbc2e018f87f66937e243e6d708
Willian Mano <willianmanoaraujo@gmail.com>
2020-07-17 19:14:12 -0300

    Fixes #244 - Incorrect license and copyright statements in metismenu Javascript

--- amd/src/metismenu.js
+++ amd/src/metismenu.js
@@ -1,25 +1,10 @@
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Metis menu definition.
- *
- * @package    theme_moove
- * @copyright  2020 Willian Mano - http://conecti.me
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+/*!
+* metismenu https://github.com/onokumus/metismenu#readme
+* A jQuery menu plugin
+* @version 2.7.8
+* @author Osman Nuri Okumus <onokumus@gmail.com> (https://github.com/onokumus)
+* @license: MIT
+*/
 define(['exports', 'jquery'], function(exports, $) {
     'use strict';
 
