cdd2fd974faff086930ad9be2021b6237f85d2c5
Willian Mano <willianmanoaraujo@gmail.com>
2022-06-16 14:58:09 -0300

    Fixes get_formatted_name error

--- version.php
+++ version.php
@@ -30,10 +30,10 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 2022061600;
+$plugin->version = 2022061700;
 
 // This is the named version.
-$plugin->release = '4.0.2';
+$plugin->release = '4.0.3';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
