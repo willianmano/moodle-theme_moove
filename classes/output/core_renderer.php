ea178daa1456b19c15df63ad5974da48d6012d2e
Willian Mano <willianmanoaraujo@gmail.com>
2022-06-18 15:47:04 -0300

    Print course summary

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -73,7 +73,7 @@ class core_renderer extends \theme_boost\output\core_renderer {
                        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
                        <link href="https://fonts.googleapis.com/css2?family='
                         . $sitefont .
-                       ':ital,wght@0,300;0,400;0,500;0,700;1,400" rel="stylesheet">';
+                       ':ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">';
 
         return $output;
     }
