3a3bb43d7d73e7f8957117000d045473547fe23c
Willian Mano <willian.mano@jove.com>
2024-03-26 14:19:46 -0300

    Fixes ReCAPTCHA issue

--- version.php
+++ version.php
@@ -30,10 +30,10 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 2023100902;
+$plugin->version = 2023100903;
 
 // This is the named version.
-$plugin->release = '4.3.2';
+$plugin->release = '4.3.3';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
