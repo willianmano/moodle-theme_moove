cb55da972acf5970e86335f3bbb61c055112837e
Willian Mano <willianmanoaraujo@gmail.com>
2025-05-17 11:46:28 -0300

    Increase version num

--- version.php
+++ version.php
@@ -30,10 +30,10 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 2025041600;
+$plugin->version = 2025041603;
 
 // This is the named version.
-$plugin->release = '5.0.2';
+$plugin->release = '5.0.3';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
